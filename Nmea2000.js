6ac17fcea9f1b1ff0ea7d8aa53d04fdbdf9ac1b9
Paul Reeve <preeve@pdjr.eu>
2021-09-25 10:35:05 +0200

    Add support for updating a single channel using PGN127502.

--- Nmea2000.js
+++ Nmea2000.js
@@ -18,4 +18,22 @@ module.exports = class Nmea2000 {
         return(retval);
     }
 
+    static makeMessagePGN127502ForChannel(instance, channel, state) {
+        var retval = "";
+        var now = (new Date()).toISOString();
+        return(now + ",6,127502,0,255,8," + Nmea2000.makeSwitchbankMessageComponentForChannel(instance, channel, state));
+    }
+
+    static makeSwitchbankMessageComponentForChannel(instance, channel, state) {
+        var channels = (new Array(28)).fill(undefined);
+        channels[channel] = state;
+        var retval = ("0" + Number(instance).toString(16)).slice(-2);
+        for (var g = 0; g < 7; g++) {
+            var v = 0;
+            for (var o = 3; o >= 0; o--) v = ((v << 2) + ((channels[(g * 4) + o] === undefined)?3:channels[(g * 4) + o]));
+            retval += "," + ("0" + Number(v).toString(16)).slice(-2);
+        }
+        return(retval);
+    }
+
 }
