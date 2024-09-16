7784664c679c8a612f4aaf4e0db6f67946615655
Paul Reeve <preeve@pdjr.eu>
2024-09-16 10:12:15 +0200

    Chore: convert to TypeScript

---
+++ Nmea2000.d.ts
@@ -0,0 +1,4 @@
+export declare class Nmea2000 {
+    static makeMessagePGN127502(instance: number, channel: number, state: number): string;
+    static makeSwitchbankMessageComponent(instance: number, channel: number, state?: number): string;
+}
